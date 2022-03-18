ab256328d068b1d9c26ef9d96b285d7c274d7952
AktamNosirov <94_aktam@mail.ru>
2022-03-18 22:24:37 +0500

    55

--- if05.py
+++ if05.py
@@ -1,13 +1,4 @@
 def main(a,b,c):
-        """
-    Find how many positive numbers there are in the given numbers.
-    Args:
-        a: integer
-        b: integer
-        c: integer
-    returns:
-        integer: the number of positive numbers in the given numbers
-    """
     if a<0 : a=1 
     else: a=0 
     if b<0 : b=1 

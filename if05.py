e9ae92d8fb023e8128b2287c75e9e2d2d4fade65
AktamNosirov <94_aktam@mail.ru>
2022-03-18 22:23:02 +0500

    5

--- if05.py
+++ if05.py
@@ -1,11 +1,18 @@
 def main(a,b,c):
-    """
-    Find how many negative numbers there are in the given numbers.
+        """
+    Find how many positive numbers there are in the given numbers.
     Args:
         a: integer
         b: integer
         c: integer
     returns:
-        integer: the number of negative numbers in the given numbers
+        integer: the number of positive numbers in the given numbers
     """
-    return
+    if a<0 : a=1 
+    else: a=0 
+    if b<0 : b=1 
+    else: b=0 
+    if c<0 : c=1 
+    else: c=0 
+    return(a+b+c)
+print(main(12,-5,-6)) 

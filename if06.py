3355911bd4e31918b8ecfab43d5e3d8e795be0aa
AktamNosirov <94_aktam@mail.ru>
2022-03-19 14:26:52 +0300

    7.5

--- if06.py
+++ if06.py
@@ -1,17 +1,21 @@
 def main(a,b,c):
-    if a>0 : a=1 
-    else: a=0 
-    if b>0 : b=1 
-    else: b=0 
-    if c>0 : c=1 
-    else: c=0   
-    ans1=a+b+c
-    ans2=3-ans1
+    ans1=0
+    ans2=0
+    if a>0 : ans1+=1 
+    elif a<0 :ans2+=1
+    else : ans1+=0
+    if b>0 : ans1+=1 
+    elif b<0 :ans2+=1 
+    else : ans1+=0
+    if c>0 : ans1+=1
+    elif c<0 :ans2+=1
+    else : ans1+=0 
     if ans1>ans2 : 
         return "there are a lot of positive numbers" ,ans1
     elif ans2>ans1 : 
         return "there are a lot of negative numbers" ,ans2
-print(main(1,-2,-3)) 
+
+print(main(0,-2,1)) 
 
 
 
--- if07.py
+++ if07.py
@@ -1,26 +1,14 @@
 def main(a):
-    """
-    Given an integer a, check the following conditions:
-    "positive odd number",
-    "positive even number",
-    "negative odd number",
-    "negative even number",
-    "the number is zero"
-
-    Args:
-        a: integer
-    Returns:
-        string: the message to print
-    """
     if a > 0 and a%2 == 1 : 
         return "positive odd number"
     elif a > 0 and a%2 == 0 : 
         return "positive even number"
     elif a < 0 and a%2 == 1 :
-         return "positive odd number"
+        return "positive odd number"
     elif a < 0 and a%2 == 0 : 
         return "positive even number"
     else : return "the number is zero"
+
 print(main(0))
 
 

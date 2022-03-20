d45bf84e78bd07157486861a5a73fe2d460324f6
AktamNosirov <94_aktam@mail.ru>
2022-03-21 00:09:41 +0500

    6

--- if06.py
+++ if06.py
@@ -8,9 +8,9 @@ def main(a,b,c):
     if c>0 : ans1+=1 
     else: ans2+=1 
     if ans1>ans2 :
-        return "there are a lot of positive numbers", ans1
+        return "there are a lot of positive numbers"
     if ans1<ans2: 
-        return "there are a lot of negative numbers" , ans2
+        return "there are a lot of negative numbers" 
 print(main(1,-2,-3))
 
 

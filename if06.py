7d8cb6d92e35dc8c57632e9ce4af762e33d27b67
AktamNosirov <94_aktam@mail.ru>
2022-03-20 13:44:16 +0300

    9

--- if06.py
+++ if06.py
@@ -1,15 +1,13 @@
 def main(a,b,c):
-    ans1=0
-    ans2=0
-    if a>0 : ans1+=1 
-    elif a<0 :ans2+=1
-    else : ans1+=0
-    if b>0 : ans1+=1 
-    elif b<0 :ans2+=1 
-    else : ans1+=0
-    if c>0 : ans1+=1
-    elif c<0 :ans2+=1
-    else : ans1+=0 
+    
+    if a>0 : a=1 
+    else : a=0
+    if b>0 : b=1 
+    else: b=0
+    if c>0 : c=1 
+    else: c=0
+    ans1=a+b+c
+    ans2=3-ans1
     if ans1>ans2 : 
         return "there are a lot of positive numbers" ,ans1
     elif ans2>ans1 : 
--- if09.py
+++ if09.py
@@ -9,4 +9,6 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    if a%10*10+a//10<=a : return bool(1) 
+    elif a%10*10+a//10>a : return bool(0)
+print(main(78))

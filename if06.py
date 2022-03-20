d009737a4d397f26d59ff9d156038cd06cef4f77
AktamNosirov <94_aktam@mail.ru>
2022-03-21 00:06:16 +0500

    6.3

--- if06.py
+++ if06.py
@@ -7,7 +7,6 @@ def main(a,b,c):
     else: ans2+=1 
     if c>0 : ans1+=1 
     else: ans2+=1 
-    d=a+b+c
     if ans1>ans2 :
         return "there are a lot of positive numbers", ans1,
     if ans1<ans2: 
